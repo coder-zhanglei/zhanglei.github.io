3b9ab7fe3c631c5164a3d7ae6dd1725b4d0c8240
zhanglei <1357028281@qq.com>
2019-04-21 22:47:31 +0800

    python plotly

--- python/test/test.py
+++ python/test/test.py
@@ -1,5 +1,28 @@
 # pyhton
-
+import os
 import requests
+import plotly as py
+import pandas as pd
+import plotly.graph_objs as pygo
+
+print os.getcwd()
+trace0 = pygo.Scatter(
+    x = [1,2,3,4],
+    y = [10,15,13,17]
+)
+
+trace1 = pygo.Scatter(
+    x = [1,2,3,4],
+    y = [16,5,11,9]
+)
+
+data = pygo.Data([trace0,trace1])
+
+#py.offline.plot(data,filename = os.getcwd() + 'first_start_introduction.html')
+
+py.offline.plot(data)
+py.offline.plot(data,filename = os.getcwd() + 'first_start_introduction.html',image='png')
+
+
 print "fsadfasf"
 
